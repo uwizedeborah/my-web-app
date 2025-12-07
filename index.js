4c7cd12f4f0520d86022b0cbacfe9acc498673d0
Uwizeyimana_Deborah <uwizeyimanadeborah5@gmail.com>
2025-12-07 12:15:21 +0200

    Work in progress

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     res.end('About Page\n');
   } else if (req.url === '/auth') {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(auth.getWelcomeMessage());
+    res.end(author.getWelcomeMessage());
   } else {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello, this is a basic web app!');
@@ -16,4 +16,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
-});
+});s
